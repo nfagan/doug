4eec0a08edbca63ed719a8f6ae7010dca7216938
nfagan <nick@fagan.org>
2016-03-18 16:03:58 -0400

    added ability to combine across monkeys

---
+++ functions/acrossMonkeys.m
@@ -0,0 +1,25 @@
+function meanSaveData = acrossMonkeys(saveData)
+
+    for i = 1:length(saveData{1});
+        for j = 1:size(saveData{1}{1},1);
+            for k = 1:size(saveData{1}{1},2);                
+                for ik = 1:length(saveData{1}{1}{1,1});
+                    for ij = 1:length(saveData{1}{1}{1,1}{1});                        
+                        fieldNames = fieldnames(saveData{1}{1}{1,1}{1}{1});                        
+                        for il = 1:length(fieldNames);
+                            storeCurrent = [];
+                            for ii = 1:length(saveData);
+                                if ii == 1;
+                                    storeCurrent = saveData{ii}{i}{j,k}{ik}{ij}.(fieldNames{il});
+                                else
+                                    update = saveData{ii}{i}{j,k}{ik}{ij}.(fieldNames{il});
+                                    storeCurrent = vertcat(storeCurrent,update);
+                                end
+                            end
+                            meanSaveData{1,i}{j,k}{ik,1}{1,ij}.(fieldNames{il}) = storeCurrent;
+                        end
+                    end
+                end
+            end
+        end
+    end
--- scripts/exampleScriptNick.m
+++ scripts/exampleScriptNick.m
@@ -1,7 +1,8 @@
 % --------------------------------
-% define global variables
+% -- define global variables --
 % --------------------------------
 
+global monkeys;
 global drugTypes;
 global dosages;
 global toExamine;
@@ -12,11 +13,11 @@ global allPos;
 global compName;
 
 % --------------------------------
-% master inputs
+% -- master inputs -- 
 % --------------------------------
 
 compName = 'nick';
-monkey = 'Coppola';
+monkeys = {'Coppola'};
 drugTypes = {'OTN_2','OT_test'};
 dosages = {'small','medium','large'};
 
@@ -37,51 +38,62 @@ wholePos.minY = 250;
 wholePos.maxY = 650;
 
 allPos = {roiPos,wholePos};
-allBlockStarts = [0 150e4 300e4 450e4 600e4 750e4 900e4 1050e4];
+% allBlockStarts = [0 150e4 300e4 450e4 600e4 750e4 900e4 1050e4];
+allBlockStarts = [0 300e4 600e4 900e4];
 allBlockEnds = allBlockStarts(:) + 60e4;
 
 allTrialTypes = {'nonConspecific','people','monkeys','outdoors','animals'}; %define the images you want to isolate
 % if toExamine is 'normalized proportion', make sure first allTrialTypes is 'scrambled'
 
 % --------------------------------
-% analysis portion
+%  -- analysis portion -- 
 % --------------------------------
 
 M = cell(length(allTrialTypes),1);
-for i = 1:length(allTrialTypes); % for each image ...
-    fprintf('\nIMAGE: %d of %d (%s)',i,length(allTrialTypes),allTrialTypes{i});
-    trialType = {allTrialTypes{i}};
-for j = 1:length(drugTypes); % for each drug ...
-    fprintf('\n\tDRUG: %d of %d (%s)',j,length(drugTypes),drugTypes{j});
-    for k = 1:length(dosages); % for each dose ...
-    fprintf('\n\t\tDOSE: %d of %d (%s)',k,length(dosages),dosages{k});
-    % --------------------------------
-    % load in files
-    % --------------------------------
-    [umbrellaDirectory,doseNames{k}] = getUmbrDir(monkey,drugTypes{j},dosages{k});
-    [allTimes,allEvents] = getFilesDoug(umbrellaDirectory); % load all files
-    % --------------------------------
-    % get all relevant data
-    % --------------------------------
-    saveData{i}{j,k} = getSaveData(allTimes,allEvents,trialType);
+M2 = cell(length(allTrialTypes),1);
+for ii = 1:length(monkeys); % for each monkey ...
+    monkey = monkeys{ii};
+    for i = 1:length(allTrialTypes); % for each image ...
+        fprintf('\nIMAGE: %d of %d (%s)',i,length(allTrialTypes),allTrialTypes{i});
+        trialType = {allTrialTypes{i}};
+        for j = 1:length(drugTypes); % for each drug ...
+            fprintf('\n\tDRUG: %d of %d (%s)',j,length(drugTypes),drugTypes{j});
+            for k = 1:length(dosages); % for each dose ...
+            fprintf('\n\t\tDOSE: %d of %d (%s)',k,length(dosages),dosages{k});
+            
+            % -- load in files --
+            
+            [umbrellaDirectory,doseNames{k}] = getUmbrDir(monkey,drugTypes{j},dosages{k});
+            [allTimes,allEvents] = getFilesDoug(umbrellaDirectory); % load all files
+            
+            % -- get all relevant data --
+            saveData{ii}{i}{j,k} = getSaveData(allTimes,allEvents,trialType);
+%             saveData{i}{j,k} = getSaveData(allTimes,allEvents,trialType);
+            end
+        end
     end
 end
-end
+
+% -- combine across monkeys --
+
+fprintf('\nCombing across monkeys ...');
+allSaveData = acrossMonkeys(saveData);
 fprintf('\nDone!\n');
 %% generate table
-toExamine = 'normalized proportion';
+toExamine = 'proportion';
 for i = 1:length(allTrialTypes);
     for j = 1:length(drugTypes);
         for k = 1:length(dosages);
             % --------------------------------
             % construct matrix and plot
             % --------------------------------
-            M{i}{j,k} = genTable(saveData{i}{j,k},region);
+            M{i}{j,k} = genTable(allSaveData{i}{j,k},region);
         end
     end
 end
+%%
 % --------------------------------
 % plot
 % --------------------------------
-storePerImage = newPlot(M,'lineType','per stim','xAxis','dose','treatNaNs','meanReplace','doseNames',...
+storePerImage = newPlot(M,'lineType','per stim','xAxis','dose','treatNaNs','default','doseNames',...
     doseNames,'limits',[],'subplotPerDrug',1);
